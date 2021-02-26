fcb08685634740facef620afde97ba3834ab5237
= <1danhvaythoi@gmail.com>
2021-02-26 16:54:36 +0700

    Thanh Đế

--- Operator/src/com/company/Main.java
+++ Operator/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+        double meal_cost = Double.parseDouble(input.nextLine());
+
+        int tip_percent = Integer.parseInt(input.nextLine());
+
+        int tax_percent = Integer.parseInt(input.nextLine());
+
+        solve(meal_cost, tip_percent, tax_percent);
+        input.close();
+    }
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double tip = meal_cost * tip_percent / 100;
+        double tax = meal_cost * tax_percent / 100;
+        long total_cost = Math.round(meal_cost + tax + tip);
+        System.out.println(total_cost);
     }
 }
